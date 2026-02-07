47d05b40561a4bf71a8257c8257f286a9f9b268a
brudalevante <170347513+brudalevante@users.noreply.github.com>
2026-02-07 19:21:14 +0100

    Update WiFi initialization script for new settings

--- base-config-setting/files/wifi-init.sh
+++ base-config-setting/files/wifi-init.sh
@@ -20,12 +20,12 @@ wifi_setup_radio()
 			if test -z "${path#*+1}"; then
 				uci set wireless.${radio}.phy='phy1'
 				uci set wireless.${radio}.htmode="${htmode:-VHT}80"
-				uci set wireless.${radio}.hwmode='11a'
+				uci set wireless.${radio}.hwmode='11be'
 				uci set wireless.${radio}.band='5g'
 			else
 				uci set wireless.${radio}.phy='phy0'
 				uci set wireless.${radio}.htmode="${htmode:-HT}20"
-				uci set wireless.${radio}.hwmode='11g'
+				uci set wireless.${radio}.hwmode='11be'
 				uci set wireless.${radio}.band='2g'
 			fi
 			uci delete wireless.${radio}.path
@@ -34,67 +34,64 @@ wifi_setup_radio()
 
 		uci -q batch <<-EOT
 			set wireless.${radio}.disabled='0'
-			set wireless.${radio}.country='CN'
-			set wireless.${radio}.channel='auto'
+			set wireless.${radio}.country='US'
+			set wireless.${radio}.hwmode='11be'
 			set wireless.${radio}.cell_density='0'
 		EOT
 
-		if [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "2g" ] || [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "5g" ]; then
-			htmode="$(uci get wireless.${radio}.htmode 2>/dev/null)"
-			if [ "${htmode//EHT}" != "${htmode}" ]; then
-				if  [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "2g" ]; then
-					uci set wireless.${radio}.channel="1"
-				else
-					uci set wireless.${radio}.channel="36"
-				fi
-			fi
+		# Configuración por banda
+		if [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "2g" ]; then
+			# BANDA 2.4GHz: Canal 6
+			uci set wireless.${radio}.channel='6'
+			
+		elif [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "5g" ]; then
+			# BANDA 5GHz: Canal 100
+			uci set wireless.${radio}.channel='100'
+			
 		elif uci get wireless.${radio}.htmode 2>/dev/null | grep -q EHT; then
-			uci set wireless.${radio}.country='DE'
-			uci set wireless.${radio}.channel='21'
+			# BANDA 6GHz: Canal 36
+			uci set wireless.${radio}.channel='36'
 			uci set wireless.${radio}.band='6g'
+		else
+			# Fallback para otras bandas
+			uci set wireless.${radio}.channel='auto'
 		fi
 
+		# Crear interfaz WiFi
 		obj=$(uci add wireless wifi-iface)
 		if test -n "$obj"; then
 			uci set wireless.$obj.device="${radio}"
 			uci set wireless.$obj.network='lan'
 			uci set wireless.$obj.mode='ap'
-			uci set wireless.$obj.ssid="${SSID}"
-			uci set wireless.$obj.encryption='psk2'
-			uci set wireless.$obj.key="${SSID_PASSWD}"
+			uci set wireless.$obj.encryption='sae'
+			uci set wireless.$obj.key='12345678'
+			
+			# SSID según banda
+			if [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "6g" ]; then
+				uci set wireless.$obj.ssid="BANANA_6G"
+				uci set wireless.$obj.ocv='0'
+				
+			elif [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "5g" ]; then
+				uci set wireless.$obj.ssid="BANANA_5G"
+				
+			elif [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "2g" ]; then
+				uci set wireless.$obj.ssid="BANANA_2G"
+			else
+				uci set wireless.$obj.ssid="BANANA"
+			fi
+			
+			# IEEE 802.11r (Fast Roaming) - Solo si no es bcma o Cypress
 			if uci get wireless.${radio}.path | grep -q bcma || iwinfo wlan${radio:5} info | grep -qi Cypress; then
-				if [ "$(uci get wireless.${radio}.band)" = "2g" ]; then
-					uci set wireless.${radio}.channel='1'
-				else
-					uci set wireless.${radio}.channel='36'
-				fi
+				:
 			else
 				uci set wireless.$obj.ieee80211r='1'
 				uci set wireless.$obj.ft_over_ds='0'
 				uci set wireless.$obj.ft_psk_generate_local='1'
 			fi
-
-
+			
+			# Eliminar ft_psk_generate_local en 6GHz (incompatible con SAE)
 			if [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "6g" ]; then
-				uci set wireless.$obj.encryption='sae'
-				uci set wireless.$obj.ocv='0'
-				uci set wireless.$obj.ssid="${SSID}_6G"
-				uci delete wireless.$obj.ft_psk_generate_local
-			fi
-			if [ "$(uci get wireless.${radio}.band 2>/dev/null)" = "5g" ] && test -e /sys/kernel/debug/ieee80211/phy0/mt76; then
-				obj=$(uci add wireless wifi-iface)
-				if test -n "$obj"; then
-					uci set wireless.$obj.device="${radio}"
-					uci set wireless.$obj.network='lan'
-					uci set wireless.$obj.mode='ap'
-					uci set wireless.$obj.ssid="${SSID}_5G"
-					uci set wireless.$obj.encryption='psk2'
-					uci set wireless.$obj.key="${SSID_PASSWD}"
-					uci set wireless.$obj.ieee80211r='1'
-					uci set wireless.$obj.ft_over_ds='0'
-					uci set wireless.$obj.ft_psk_generate_local='1'
-					uci set wireless.$obj.subfix="_5G"
-				fi
+				uci delete wireless.$obj.ft_psk_generate_local 2>/dev/null
 			fi
 		fi
 	fi
@@ -102,9 +99,6 @@ wifi_setup_radio()
 
 wifi_first_init()
 {
-	SSID="${SSID-$(uci get base_config.@status[0].SSID 2>/dev/null || echo X-WRT)}"
-	SSID_PASSWD="${SSID_PASSWD-$(uci get base_config.@status[0].SSID_PASSWD 2>/dev/null || echo 88888888)}"
-
 	while uci delete wireless.@wifi-iface[0] >/dev/null 2>&1; do :; done
 	for radio in radio0 radio1 radio2 radio3 wifi0 wifi1 wifi2 wifi3; do
 		wifi_setup_radio ${radio}
